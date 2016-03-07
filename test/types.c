08a22d44c01287a0be79abadca11675b26e2b29f
Moritz Koenig <if62yvyf@stud.informatik.uni-erlangen.de>
2016-03-07 14:28:30 +0100

    3

---
+++ test/types.c
@@ -0,0 +1,16 @@
+int a1; {{A}}
+char a2; {{B}}
+short a3; {{C}}
+
+long a2; {{A}}
+long long a3; {{B}}
+float a1; {{C}}
+
+double a3; {{A}}
+long double a1; {{B}}
+unsigned short a2; {{C}}
+/*
+ * check-name: Definition of Global Variable
+ * B != A, C != B, A != C
+ */
+
