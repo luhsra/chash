a83c6727aa1d684970e49231260c382a752026b4
Lukas Lehnert <or30emiq@stud.informatik.uni-erlangen.de>
2016-03-07 13:56:15 +0100

    2

---
+++ test/functions/global_builtin_function.c
@@ -0,0 +1,40 @@
+
+{{A:char}}{{B:long}}second;
+
+
+int callee(int); {{A}}
+int callee(int); {{B}}
+
+void caller(void){ {{A}}
+void caller(void){ {{B}}
+
+	int a = 8; {{A}}
+	int a = 8; {{B}}
+	callee(a); {{A}}
+	callee(a); {{B}}
+} {{A}}
+} {{B}}
+
+int callee(int a){ {{A}}
+int callee(int a){ {{B}}
+	return a + 1; {{A}}
+	return a - 1; {{B}}
+} {{A}}
+} {{B}}
+
+
+int main(){ {{A}}
+int main(){ {{B}}
+	
+	caller(); {{A}}
+	caller(); {{B}}
+
+} {{A}}
+} {{B}}
+
+
+/*
+ * check-name: Definition of Global Variable
+ * A == C, B != A
+ */
+
