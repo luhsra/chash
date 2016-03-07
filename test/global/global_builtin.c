9f7960d6cd374f14d514e4268c22f9db36b451b8
Lukas Lehnert <or30emiq@stud.informatik.uni-erlangen.de>
2016-03-07 14:32:53 +0100

    bisschen aufgerauemt

---
+++ test/global/global_builtin.c
@@ -0,0 +1,11 @@
+int first; {{A}}
+int first; {{C}}
+unsigned int first; {{B}}
+
+{{A:char}}{{B:long}}{{C:char}} second;
+
+typedef int unused; {{C}}
+/*
+ * check-name: Definition of Global Variable
+ * A == C, B != A
+ */
---
+++ test/global/global_builtin_Z.c
@@ -0,0 +1,11 @@
+int first = 0; {{A}}
+int first = 1; {{C}}
+unsigned int first = 0; {{B}}
+
+{{A:char}}{{B:long}}{{C:char}} second;
+
+typedef int unused; {{C}}
+/*
+ * check-name: Definition of Global Variable
+ * A == C, B != A
+ */
