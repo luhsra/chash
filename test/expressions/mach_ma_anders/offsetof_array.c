dab0c6ae897c5f09d689a9e20de879725fb9e463
Lukas Lehnert <or30emiq@stud.informatik.uni-erlangen.de>
2016-03-11 14:49:10 +0100

    offsetof-testcases angepasst

--- test/expressions/mach_ma_anders/offsetof_array.c
+++ test/expressions/mach_ma_anders/offsetof_array.c
@@ -1,8 +1,15 @@
-int array[10];
+#include <stddef.h>
 
-size_t a = offsetof(array, array[2]); {{A}}
-size_t a = 8; {{B}}
+/*This does only make sense in an array context*/
+struct ding{
+	int b;
+	int array[10];
+};
 
+void func(void){
+	unsigned int a = offsetof(struct ding, array[2]); {{A}}
+	unsigned int a = 8; {{B}}
+}
 /*
  * check-name: offsetof array
  * obj-not-diff: might
--- test/expressions/mach_ma_anders/offsetof_struct.c
+++ test/expressions/mach_ma_anders/offsetof_struct.c
@@ -1,10 +1,15 @@
+#include <stddef.h>
+
+
 struct test{
 	int a1;
 	int a2;
-}
+};
 
-size_t a = offsetof(test, a2); {{A}}
-size_t a = 4; {{B}}
+void func(){
+	unsigned int a = offsetof(struct test, a2); {{A}}
+	unsigned int a = 4; {{B}}
+}
 
 /*
  * check-name: offsetof struct
