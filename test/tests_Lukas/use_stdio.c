86b8b231894969f0ff32a1f3428c3c89c18840b0
Lukas Lehnert <or30emiq@stud.informatik.uni-erlangen.de>
2016-03-14 11:45:31 +0100

    nochmal include tests

---
+++ test/tests_Lukas/use_stdio.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+
+	printf("Hello World\n"); {{A}}
+	printf("Ciao World\n"); {{B}}
+
+}
+/*
+ * check-name: use stdio.h
+ * A != B
+ */
---
+++ test/tests_Lukas/useless_dynamic_includes.c
@@ -0,0 +1,9 @@
+#include <stdio.h> {{A}}
+#include <stdlib.h> {{B}}
+
+{{C}}
+
+/*
+ * check-name: not used dynamic include
+ * A == B, B == C
+ */
