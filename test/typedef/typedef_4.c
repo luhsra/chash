8ce330826611d9a43b5e07c88d4fdf246d40b8dc
Ludwig Fueracker <ludwig.fueracker@fau.de>
2016-06-14 17:04:36 +0200

    improved typedef testcase

--- test/typedef/typedef_4.c
+++ test/typedef/typedef_4.c
@@ -1,11 +1,12 @@
 typedef const int used;
 
-used first; {{A}}
-int first;  {{B}}
+used first;      {{A}}
+int first;       {{B}}
+const int first; {{C}}
 
 /*
  * check-name: typedef const
  * obj-not-diff: yes
- * assert-ast: A != B
- * assert-obj: A == B
+ * assert-ast: A != B, B != C, A == C
+ * assert-obj: A == B, B == C
  */
