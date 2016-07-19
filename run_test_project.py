7d71c7e0bd29eb7e91b7430b7a1fef9268d75d70
Ludwig Fueracker <ludwig.fueracker@fau.de>
2016-07-19 15:39:19 +0200

    added objfile-size and total build time

--- run_test_project.py
+++ run_test_project.py
@@ -5,9 +5,11 @@ import re
 import subprocess
 from subprocess import check_output
 import datetime
+import time
 import sys
 
 #TODO: make paths independent => make project path command line argument!
+projectName = "musl"
 pathToProject = os.path.abspath("../hash_projects/musl")
 clanghashWrapper = os.path.abspath("build/wrappers/clang")
 
@@ -44,12 +46,13 @@ if (len(sys.argv) > 1):
 log("Starting at %s" % datetime.datetime.now())
 
 os.environ['CC'] = clanghashWrapper
-os.environ['PROJECT'] = "musl" 
+os.environ['PROJECT'] = projectName
 
 #reset to latest version
 checkout("master")
 
 commitCounter = 0
+buildTimes = {}
 for commitID in getListOfCommits():
     os.environ['COMMIT_HASH'] = commitID
     log ("hashing commit #%d" % commitCounter)
@@ -60,16 +63,21 @@ for commitID in getListOfCommits():
     checkout(commitID)
     subprocess.call(["./configure"])
     log("calling make -j16")
+    startTime = time.time()
     p = subprocess.Popen(["make", "-j16"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
     retcode = p.wait()
-
+    buildTimes[commitID] = (time.time() - startTime) * 10e9 # nano
     commitCounter += 1
     log("finished commit %s at %s" % (commitID, datetime.datetime.now()))
-
     if (commitsToHash > 0 and commitCounter >= commitsToHash):
         break;
 
+#TODO: replace absolute path
+
+f = open("/home/cip/2015/yb90ifym/clang-hash/build/muslHashes/buildTimes_%s.info" % projectName, 'a')
+f.write(repr(buildTimes) + "\n")
+f.close()
 log("Finished at %s" % datetime.datetime.now())
 log("Hashed commits: %d" % commitCounter)
 
