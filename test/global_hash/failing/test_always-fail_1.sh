5e2e64ddabb138c224e984cf50c6e908a3d07ce5
Ludwig Fueracker <ludwig.fueracker@fau.de>
2017-04-12 11:49:45 +0200

    Replace absolute paths with relative paths

--- test/global_hash/failing/test_always-fail_1.sh
+++ test/global_hash/failing/test_always-fail_1.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#this=$(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0)
-#dir=$(dirname "${this}")
-#. "$dir/global_hash.sh"
-source "/home/cip/2015/yb90ifym/clang-hash/test/global_hash/global_hash.sh"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "$DIR/../global_hash.sh"
 
 # check-name: clang-hash-global failing testcase #1: false positive (function)
 # check-known-to-fail: true
--- test/global_hash/trivial/test_simple_change_4.sh
+++ test/global_hash/trivial/test_simple_change_4.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-source "/home/cip/2015/yb90ifym/clang-hash/test/global_hash/global_hash.sh"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "$DIR/../global_hash.sh"
 
 # check-name: clang-hash-global simple change #4 (variable)
 
