8134a1a0b47efe7273527231eebbb9dd46251858
Simon van Schwartzenberg <lexx12@live.de>
2017-07-27 13:47:15 +0200

    .

--- gcc-plugin/chash.cc
+++ gcc-plugin/chash.cc
@@ -12,7 +12,7 @@
 
 #include <algorithm>
 #include <assert.h>
-
+#include "Hash.h"
 
 #include "gcc-common.h"
 #include "chash.h"
@@ -40,6 +40,7 @@
 #include "profile.h"
 #include "tree-cfgcleanup.h"
 #include "params.h"
+
 
 int plugin_is_GPL_compatible;
 
@@ -75,177 +76,121 @@ struct cgraph_node *get_fn_cnode(const_tree fndecl)
  */
 extern "C" void chash_ast_hash_execute(void *gcc_data, void*) {
     tree fndecl = (tree)gcc_data;
-    gcc_assert (TREE_CODE (fndecl) == FUNCTION_DECL);
 
-    printf("-----------------------------------------------------\n");
-    debug_tree(fndecl); //== \/
-    printf("****\n");
-    debug_tree(DECL_SAVED_TREE (fndecl));
-
-        // enum tree_code code = TREE_CODE(node->decl);
-        // if(code==FUNCTION_DECL){
-        //     tree func = node->decl,                     //function decl
-        //         rType = TREE_TYPE(DECL_RESULT(func)),   
-        //         arguments = DECL_ARGUMENTS(func),
-        //         body = DECL_SAVED_TREE(func);
-        //         //if (!body) body = DECL_INITIAL(func);       
-        //     printf("Function decl:\n");
-        //     printf("Name:\t\t\t\t\t%s\n", IDENTIFIER_POINTER(DECL_NAME(func)));
-        //     printf("Type of return value:\t\t\t%s\n", get_tree_code_name(TREE_CODE(rType)));
-        //     printf("%s\n", body==0?"leer":"nicht leer");
-        //     if(body != 0){
-        //         printf("****\n");
-        //         debug_tree(body);
-                
-        //         //printf(":\t\t\t%s\n", get_tree_code_name(TREE_CODE(body)));
-        //         tree chain = BLOCK_VARS(body);
-        //         chain = chain ? BLOCK_CHAIN(chain) : 0;
-        //         printf("%s\n", chain == 0 ? "no chain": "chain");
-        //         while (chain != 0){
-        //             printf("%s %p ", get_tree_code_name(TREE_CODE(chain)), chain);
-        //             chain = TREE_CHAIN(chain);
-        //         }
-        //         printf("\n");
-        //     }
-            
-
-        //     while(1){ // Will only run once. Special attributes.
-        //         if (DECL_FUNCTION_SPECIFIC_TARGET (func)){
-        //             printf("%s\n", "1");
-        //             break;
-        //         }
-        //         if (DECL_FUNCTION_SPECIFIC_OPTIMIZATION (func)){
-        //             printf("%s\n", "2");
-        //             break;
-        //         }
-        //         if (DECL_DECLARED_INLINE_P (func)){
-        //             printf("%s\n", "inlined");
-        //             break;
-        //         }
-        //         if (DECL_BUILT_IN (func)){
-        //             printf("%s\n", "4");
-        //             break;
-        //             }
-        //         if (DECL_STATIC_CHAIN (func)){
-        //             printf("%s\n", "5");
-        //             break;
-        //         }
-        //         if (decl_is_tm_clone (func)){
-        //             printf("%s\n", "6");
-        //             break;
-        //         }
-        //         printf("No special Attributes\n");
-        //         break;
-        //     }
-
-        //     // while(body != 0 ){
-        //     //     printf(":\t\t\t%s\n", IDENTIFIER_POINTER(DECL_NAME(body)));
-        //     //     body = TREE_CHAIN(body);
+    int code = TREE_CODE(fndecl);
+    if(code==FUNCTION_DECL){
+        tree rType = TREE_TYPE(DECL_RESULT(fndecl)),   
+            arguments = DECL_ARGUMENTS(fndecl),
+            body = DECL_SAVED_TREE(fndecl);
+        printf("Function decl:\n");
+        printf("Name:\t\t\t\t\t%s\n", IDENTIFIER_POINTER(DECL_NAME(fndecl)));
+        printf("Type of return value:\t\t\t%s\n", get_tree_code_name(TREE_CODE(rType)));
+        printf("%s\n", IDENTIFIER_POINTER(DECL_NAME(TREE_OPERAND(STATEMENT_LIST_HEAD(body)->stmt, 0))));
+        // printf("%s\n", IDENTIFIER_POINTER(DECL_NAME(TREE_OPERAND(STATEMENT_LIST_TAIL(body)->stmt, 0))));
+        tree_statement_list_node* tmp= STATEMENT_LIST_HEAD(body);
+        while(tmp!=0){
+            printf("%s\n", tmp == 0 ? "a": "b");
+            tmp=tmp->next;
+        }
+        printf("%s\n", STATEMENT_LIST_HEAD(body)->next->next == 0 ? "c": "d");
+        // printf("%s\n", get_tree_code_name(TREE_CODE(TREE_OPERAND(STATEMENT_LIST_HEAD(body)->next->next->stmt, 0))));
+        // debug_tree(body);
+        // debug_tree(COMPOUND_STMT(body));
+        // if(body != 0){
+        //     printf("****\n");
+        //     //printf(":\t\t\t%s\n", get_tree_code_name(TREE_CODE(body)));
+        //     // tree chain = BLOCK_VARS(body);
+        //     // // chain = chain ? BLOCK_CHAIN(chain) : 0;
+        //     // printf("%s\n", chain == 0 ? "no chain": "chain");
+        //     // while (chain != 0){
+        //     //     printf("%s %p ", get_tree_code_name(TREE_CODE(chain)), chain);
+        //     //     chain = TREE_CHAIN(chain);
         //     // }
-        //     while(arguments!=0){ // Parse all arguments of the function
-        //         printf("%s\n", get_tree_code_name(TREE_CODE(DECL_ARG_TYPE(arguments))));
-        //         arguments = TREE_CHAIN(arguments);
-        //     }
+        //     printf("\n");
         // }
+        
+
+        while(1){ // Will only run once. Special attributes.
+            if (DECL_FUNCTION_SPECIFIC_TARGET (fndecl)){
+                printf("%s\n", "1");
+                break;
+            }
+            if (DECL_FUNCTION_SPECIFIC_OPTIMIZATION (fndecl)){
+                printf("%s\n", "2");
+                break;
+            }
+            if (DECL_DECLARED_INLINE_P (fndecl)){
+                printf("%s\n", "inlined");
+                break;
+            }
+            if (DECL_BUILT_IN (fndecl)){
+                printf("%s\n", "4");
+                break;
+                }
+            if (DECL_STATIC_CHAIN (fndecl)){
+                printf("%s\n", "5");
+                break;
+            }
+            if (decl_is_tm_clone (fndecl)){
+                printf("%s\n", "6");
+                break;
+            }
+            printf("No special Attributes\n");
+            break;
+        }
+
+        // while(body != 0 ){
+        //     printf(":\t\t\t%s\n", IDENTIFIER_POINTER(DECL_NAME(body)));
+        //     body = TREE_CHAIN(body);
+        // }
+        while(arguments!=0){ // Parse all arguments of the fndecltion
+            printf("%s\n", get_tree_code_name(TREE_CODE(DECL_ARG_TYPE(arguments))));
+            arguments = TREE_CHAIN(arguments);
+        }
+    }
         // // int dExt = DECL_EXTERNAL(node->decl);
-        // // printf("Function is undefined?\t\t\t%s\n", dExt == 1 ? "true" : dExt == 0 ? "false" : "cHash Error");
+        // // printf("fndecltion is undefined?\t\t\t%s\n", dExt == 1 ? "true" : dExt == 0 ? "false" : "cHash Error");
         // // int tPub = TREE_PUBLIC(node->decl);
-        // // printf("Function has external linkage?\t\t%s\n", tPub == 1 ? "true" : tPub == 0 ? "false" : "cHash Error");
+        // // printf("fndecltion has external linkage?\t\t%s\n", tPub == 1 ? "true" : tPub == 0 ? "false" : "cHash Error");
         // // int tStatic = TREE_STATIC(node->decl);
-        // // printf("Function has been defined?\t\t%s\n", tStatic == 1 ? "true" : tStatic == 0 ? "false" : "cHash Error");
+        // // printf("fndecltion has been defined?\t\t%s\n", tStatic == 1 ? "true" : tStatic == 0 ? "false" : "cHash Error");
         // // int tVol = TREE_THIS_VOLATILE(node->decl);
-        // // printf("Function does not return normally?\t%s\n", tVol == 1 ? "true" : tVol == 0 ? "false" : "cHash Error");
+        // // printf("fndecltion does not return normally?\t%s\n", tVol == 1 ? "true" : tVol == 0 ? "false" : "cHash Error");
         // // int tRO = TREE_READONLY(node->decl);
-        // // printf("Function can only read its arguments?\t%s\n", tRO == 1 ? "true" : tRO == 0 ? "false" : "cHash Error");
+        // // printf("fndecltion can only read its arguments?\t%s\n", tRO == 1 ? "true" : tRO == 0 ? "false" : "cHash Error");
         // // int dPureP = DECL_PURE_P(node->decl);
-        // // printf("Function can only read its arguments, but may read globally?\t%s\n", dPureP == 1 ? "true" : dPureP == 0 ? "false" : "cHash Error");
+        // // printf("fndecltion can only read its arguments, but may read globally?\t%s\n", dPureP == 1 ? "true" : dPureP == 0 ? "false" : "cHash Error");
         // // int dVirP = DECL_VIRTUAL_P(node->decl);
-        // // printf("Function is virtual?\t\t\t%s\n", dVirP == 1 ? "true" : dVirP == 0 ? "false" : "cHash Error");
+        // // printf("fndecltion is virtual?\t\t\t%s\n", dVirP == 1 ? "true" : dVirP == 0 ? "false" : "cHash Error");
         // // int dArt = DECL_ARTIFICIAL(node->decl);
-        // // printf("Function was implicitly generated by the compiler, rather than explicitly declared. In addition to implicitly generated class member functions, this macro holds for the special functions created to implement static initialization and destruction, to compute run-time type information, and so forth.\t%s\n", dArt == 1 ? "true" : dArt == 0 ? "false" : "o no");
-        // // printf("%p\n", DECL_FUNCTION_SPECIFIC_TARGET(node->decl)); // ??
-        // // printf("%p\n", DECL_FUNCTION_SPECIFIC_OPTIMIZATION(node->decl)); // ??
-        // // printf("Mangled name of the function.\t\t%p\n", DECL_ASSEMBLER_NAME(node->decl)); // ??
+        // // printf("fndecltion was implicitly generated by the compiler, rather than explicitly declared. In addition to implicitly generated class member fndecltions, this macro holds for the special fndecltions created to implement static initialization and destruction, to compute run-time type information, and so forth.\t%s\n", dArt == 1 ? "true" : dArt == 0 ? "false" : "o no");
+        // // printf("%p\n", DECL_fndeclTION_SPECIFIC_TARGET(node->decl)); // ??
+        // // printf("%p\n", DECL_fndeclTION_SPECIFIC_OPTIMIZATION(node->decl)); // ??
+        // // printf("Mangled name of the fndecltion.\t\t%p\n", DECL_ASSEMBLER_NAME(node->decl)); // ??
         // // printf("First parameter type.\t\t\t%p\n", DECL_ARGUMENTS(node->decl)); // ??
         // // printf("First parameter type.\t\t\t%p\n", DECL_ARGUMENTS(node->decl)); // ??
-        // // printf("Function return type.\t\t\t%p\n", DECL_RESULT(node->decl)); // ??
-        // // printf("Function return type.\t\t\t%p\n", TYPE_CANONICAL(DECL_RESULT(node->decl))); // ??
+        // // printf("fndecltion return type.\t\t\t%p\n", DECL_RESULT(node->decl)); // ??
+        // // printf("fndecltion return type.\t\t\t%p\n", TYPE_CANONICAL(DECL_RESULT(node->decl))); // ??
         // // woher bekomme ich vergleichswerte?
-        // // printf("null: %i, enum: %i, bool: %i, int: %i, real: %i, pointer: %i, reference: %i, fixed point: %i, complex: %i, void: %i, function: %i\n", NULL_TREE, ENUMERAL_TYPE, BOOLEAN_TYPE, INTEGER_TYPE, REAL_TYPE, POINTER_TYPE, REFERENCE_TYPE, FIXED_POINT_TYPE, COMPLEX_TYPE, VOID_TYPE, FUNCTION_TYPE);
-        // // printf("Function body.\t\t\t\t%p\n", DECL_SAVED_TREE(node->decl)); // ??
-        // // printf("Function type.\t\t\t\t%p\n", &(TREE_TYPE(node->decl))); // ??
+        // // printf("null: %i, enum: %i, bool: %i, int: %i, real: %i, pointer: %i, reference: %i, fixed point: %i, complex: %i, void: %i, fndecltion: %i\n", NULL_TREE, ENUMERAL_TYPE, BOOLEAN_TYPE, INTEGER_TYPE, REAL_TYPE, POINTER_TYPE, REFERENCE_TYPE, FIXED_POINT_TYPE, COMPLEX_TYPE, VOID_TYPE, fndeclTION_TYPE);
+        // // printf("fndecltion body.\t\t\t\t%p\n", DECL_SAVED_TREE(node->decl)); // ??
+        // // printf("fndecltion type.\t\t\t\t%p\n", &(TREE_TYPE(node->decl))); // ??
         // printf("\n");
 
        
         //debug_tree(((union tree_node *)(const union tree_node *)(node->decl)));
     //}
-
-/*
-DECL_EXTERNAL
-
-    This predicate holds if the function is undefined.
-TREE_PUBLIC
-
-    This predicate holds if the function has external linkage.
-TREE_STATIC
-
-    This predicate holds if the function has been defined.
-TREE_THIS_VOLATILE
-
-    This predicate holds if the function does not return normally.
-TREE_READONLY
-
-    This predicate holds if the function can only read its arguments.
-DECL_PURE_P
-
-    This predicate holds if the function can only read its arguments, but may also read global memory.
-DECL_VIRTUAL_P
-
-    This predicate holds if the function is virtual.
-DECL_ARTIFICIAL
-
-    This macro holds if the function was implicitly generated by the compiler, rather than explicitly declared. In addition to implicitly generated class member functions, this macro holds for the special functions created to implement static initialization and destruction, to compute run-time type information, and so forth.
-DECL_FUNCTION_SPECIFIC_TARGET
-
-    This macro returns a tree node that holds the target options that are to be used to compile this particular function or NULL_TREE if the function is to be compiled with the target options specified on the command line.
-DECL_FUNCTION_SPECIFIC_OPTIMIZATION
-
-    This macro returns a tree node that holds the optimization options that are to be used to compile this particular function or NULL_TREE if the function is to be compiled with the optimization options specified on the command line.
-DECL_NAME
-
-    This macro returns the unqualified name of the function, as an IDENTIFIER_NODE. For an instantiation of a function template, the DECL_NAME is the unqualified name of the template, not something like f<int>. The value of DECL_NAME is undefined when used on a constructor, destructor, overloaded operator, or type-conversion operator, or any function that is implicitly generated by the compiler. See below for macros that can be used to distinguish these cases.
-DECL_ASSEMBLER_NAME
-
-    This macro returns the mangled name of the function, also an IDENTIFIER_NODE. This name does not contain leading underscores on systems that prefix all identifiers with underscores. The mangled name is computed in the same way on all platforms; if special processing is required to deal with the object file format used on a particular platform, it is the responsibility of the back end to perform those modifications. (Of course, the back end should not modify DECL_ASSEMBLER_NAME itself.)
-
-    Using DECL_ASSEMBLER_NAME will cause additional memory to be allocated (for the mangled name of the entity) so it should be used only when emitting assembly code. It should not be used within the optimizers to determine whether or not two declarations are the same, even though some of the existing optimizers do use it in that way. These uses will be removed over time.
-DECL_ARGUMENTS
-
-    This macro returns the PARM_DECL for the first argument to the function. Subsequent PARM_DECL nodes can be obtained by following the TREE_CHAIN links.
-DECL_RESULT
-
-    This macro returns the RESULT_DECL for the function.
-DECL_SAVED_TREE
-
-    This macro returns the complete body of the function.
-TREE_TYPE
-
-    This macro returns the FUNCTION_TYPE or METHOD_TYPE for the function.
-*/
 }
 
-//#define PASS_NAME chash_ast_hash
-//#define NO_GATE
-//#include "gcc-generate-simple_ipa-pass.h"
 
 void hash_expr(tree expr) {
     if (TREE_CODE(expr) == BIND_EXPR) {
-        hash_type(TREE_TYPE(expr));
+        TREE_TYPE(expr);
         
     }
 }
 /*
- * Initialization function of this plugin: the very heart & soul.
+ * Initialization fndecltion of this plugin: the very heart & soul.
  */
 int plugin_init(struct plugin_name_args *info, struct plugin_gcc_version *version)
 {
